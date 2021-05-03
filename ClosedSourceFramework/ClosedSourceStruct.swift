e41c73da5c5f8538f076cf316cb3fb0f1c95660f
Gerson Noboa <gerson.noboa@glia.com>
2021-05-03 18:10:44 +0300

    Initial commit

---
+++ ClosedSourceFramework/ClosedSourceStruct.swift
@@ -0,0 +1,12 @@
+import Alamofire
+import Foundation
+
+public struct ClosedSourceStruct {
+    public let string = "ClosedSourceFramework is providing this"
+    
+    public func makeRequest() {
+        AF.request("https://www.google.com").response { data in
+            print("Closed source framework has made a request")
+        }
+    }
+}
