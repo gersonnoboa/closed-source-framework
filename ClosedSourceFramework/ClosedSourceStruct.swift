9f842e426f279ef54c031edb516d994a747adf70
Gerson Noboa <gerson.noboa@glia.com>
2021-05-03 18:30:00 +0300

    Fix no accessible initializers error

--- ClosedSourceFramework/ClosedSourceStruct.swift
+++ ClosedSourceFramework/ClosedSourceStruct.swift
@@ -4,6 +4,10 @@ import Foundation
 public struct ClosedSourceStruct {
     public let string = "ClosedSourceFramework is providing this"
     
+    public init() {
+        
+    }
+
     public func makeRequest() {
         AF.request("https://www.google.com").response { data in
             print("Closed source framework has made a request")
